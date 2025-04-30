b25699cfd9529b0b246731b56a4885a0b90cea75
Dorian <dorian.kosanetzki001@stud.fh-dortmund.de>
2025-04-30 15:37:13 +0200

    3d-Addition for signal detection

--- DataGeneration.py
+++ DataGeneration.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import invgauss
 
 class DataGenerator:
     #################################################
@@ -11,9 +12,13 @@ class DataGenerator:
     t_step  = 0.01
 
     # Flow and concentration profile
-    v_0 = 10
+    v_0 = 0.00707 # 3ml/min in m/s
     dz  = 0.05
     c_0 = 1
+
+    #Receiver constants
+    z_depth = 0.0075
+    z_offset = 9.5
 
     # Variation of the receiver position
     # see subfunction sub_ReceiverPosition()
@@ -35,9 +40,13 @@ class DataGenerator:
                 self.t_step  = 0.01
 
                 # Flow and concentration profile
-                self.v_0 = 10
+                self.v_0 = 0.001
                 self.dz  = 0.05
                 self.c_0 = 1
+
+                # Receiver constants
+                self.z_depth = 0.05
+                self.z_offset = 10
 
                 # Variation of the receiver position
                 if f_rx == None:
@@ -54,24 +63,27 @@ class DataGenerator:
 
                 self.bit_rate = 1
                 print("Generator Ready")
+
     def sub_ReceiverPosition(self, t):
         # Parameters of varying receiver position
-        z_offset = 10
         z_ampl   =  2
         f_Rx     =  self.f_rx
         
         # Generation of varying receiver position
-        z_varyRx = z_ampl * np.sin(2*np.pi*f_Rx * t) + z_offset
+        z_varyRx = z_ampl * np.sin(2*np.pi*f_Rx * t) + self.z_offset
         
         # Generation of static receiver position for reference
-        z_statRx = z_offset * np.ones(t.shape)
+        z_statRx = self.z_offset * np.ones(t.shape)
+
+        z_depth_vector = np.arange(0, self.z_depth, (self.z_depth/self.v_0)/self.t_step)
         
-        return [z_varyRx, z_statRx]
+        return [z_varyRx, z_statRx, z_depth_vector]
 
     def sub_ReceivedSignal(self, t, z_Rx, dz, v_0, c_0, bit_sequence):
         s = np.zeros(t.shape)
         for bit in range(len(bit_sequence)):
             if bit_sequence[bit] > 0.5:
+
                 I_Reg2  = (t-bit >= (z_Rx + (dz/2))/v_0)
                 I_Reg23 = (t-bit >= (z_Rx - (dz/2))/v_0)
                 I_Reg3  = I_Reg23 & ~(I_Reg2)
@@ -81,8 +93,24 @@ class DataGenerator:
                 s += bit_contribution
         
         return s
+    
+    def sub_ReceivedSignal_3DReceiver(self, t, z_Rx, z_depth_vector, dz, v_0, c_0, bit_sequence):
+        s = np.zeros(t.shape)
+        for bit in range(len(bit_sequence)):
+            if bit_sequence[bit] > 0.5:
+                #3D Erfassung des Signals über VolumenReceiver
+                for z in z_depth_vector:
+                    I_Reg2  = (t-bit >= (z_Rx + z + (dz/2))/v_0)
+                    I_Reg23 = (t-bit >= (z_Rx + z - (dz/2))/v_0)
+                    I_Reg3  = I_Reg23 & ~(I_Reg2)
+                    bit_contribution = np.zeros(t.shape)
+                    bit_contribution[I_Reg3] = c_0 * (1 - ( z_Rx[I_Reg3] - (dz/2) ) / ( v_0*(t[I_Reg3]-bit) ))
+                    bit_contribution[I_Reg2] = c_0 * (dz/2) / ( v_0 * (t[I_Reg2] - bit) )
+                    s += bit_contribution
+        
+        return s
 
-    def createDataSet(self, number_arrays, number_bits, unique = False):
+    def createDataSet(self, number_arrays, number_bits, unique = False,  DimReceiver = False):
         # Sample times
         t = np.arange(self.t_start, self.t_stop, self.t_step)
         if unique:
@@ -93,24 +121,32 @@ class DataGenerator:
         dist_sequenzes = []
         ideal_sequenzes = []
         for seq in sequenzes:
-        
-            # Receiver position
-            z_varyRx, z_statRx = self.sub_ReceiverPosition(t)
+            
 
-            # Received signal (with/without varying Rx z-position) without noise
-            s_varyRx = self.sub_ReceivedSignal(t, z_varyRx, self.dz, self.v_0, self.c_0, seq)
-            s_statRx = self.sub_ReceivedSignal(t, z_statRx, self.dz, self.v_0, self.c_0, seq)
+            # Receiver position
+            z_varyRx, z_statRx, z_depth_vector = self.sub_ReceiverPosition(t)
+
+            if DimReceiver:
+                # Received signal (with/without varying Rx z-position) without noise
+                s_varyRx = self.sub_ReceivedSignal_3DReceiver(t, z_varyRx, z_depth_vector, self.dz, self.v_0, self.c_0, seq)
+                s_statRx = self.sub_ReceivedSignal_3DReceiver(t, z_statRx, z_depth_vector, self.dz, self.v_0, self.c_0, seq)
+            else:
+                # Received signal (with/without varying Rx z-position) without noise
+                s_varyRx = self.sub_ReceivedSignal(t, z_varyRx, self.dz, self.v_0, self.c_0, seq)
+                s_statRx = self.sub_ReceivedSignal(t, z_statRx, self.dz, self.v_0, self.c_0, seq)
 
             # Received signal superimposed with noise
-            rng = np.random.default_rng()
+            #rng = np.random.default_rng()
+            mu = 0.145
+            noise = invgauss.rvs(mu, size=t.shape)
             #noise = 0.0005 * rng.normal(size=t.shape)
-            noise = 0.0001 * rng.normal(size=t.shape)
+            #noise = 0.0001 * noise
             s_disturbed = s_varyRx + noise
 
-            # Ideal signal (static receiver and without noise)
-            s_ideal     = s_statRx + noise 
+            # Ideal signal (static receiver and without noise !=/)
+            s_ideal     = s_statRx + noise
 
-
+            # Normalization of the signals
             s_disturbed = [float(i)/max(s_disturbed) for i in s_disturbed]
             s_ideal = [float(i)/max(s_ideal) for i in s_ideal]
 
@@ -121,11 +157,12 @@ class DataGenerator:
 
         return [t, dist_sequenzes, ideal_sequenzes, sequenzes]
     
-    def plot_a_sequence(self):
-        [t, dist_sequenzes, ideal_sequenzes,sequenzes] = self.createDataSet(self.N, self.M)
 
-        s_disturbed = dist_sequenzes[0]
-        s_ideal     = ideal_sequenzes[0]
+    def plot_a_sequence(self):
+        [t, dist_sequenzes, ideal_sequenzes,sequenzes] = self.createDataSet(self.N, self.M, unique= True, DimReceiver=True)
+
+        s_disturbed = dist_sequenzes[5]
+        s_ideal     = ideal_sequenzes[5]
 
 
         # Plot both received signals (disturbed and ideal)
@@ -136,11 +173,45 @@ class DataGenerator:
         plt.ylabel('Received signal s')
         plt.show()
 
+'''
+    def sub_PointSourceSignal(self, t, x, v_0, D, c_0):
+        """ Modelliert eine Punktquelle in der Mitte, advektiert und diffundiert über die Zeit und Position """
+        # Berechnung der Konzentration C(x, t) basierend auf der Advektions-Diffusions-Gleichung
+        C = np.zeros((len(x), len(t)))
+        for i,x_point in enumerate(x):
+            C[i] = (c_0 / (np.sqrt(4 * np.pi * D * t + 1e-12))) * np.exp(-((x_point- v_0 * t) ** 2) / (4 * D * t + 1e-12))
+        return C
+
+    def plot_point_source(self):
+        """ Plotte die Punktquelle, die sich bewegt und diffundiert über Zeit und Position """
+        # Zeit- und Positionsvektoren
+        t = np.arange(self.t_start, self.t_stop, self.t_step)
+        x = np.linspace(0, 0.05, 200)  # Kanal mit Länge 5 cm (0.05 m)
+
+        # Diffusionskoeffizient
+        D = 4.0e-12  # m²/s
+
+        # Berechnung des Signals
+        signal = self.sub_PointSourceSignal(t, x, self.v_0, D, self.c_0)
+        print(signal.shape)
+        print(signal[0:5, 0:5])
+        # Plotten des Signals als 2D-Diagramm
+        plt.figure(figsize=(10, 6))
+        plt.contourf(t, x, signal, levels=50, cmap='viridis')
+        plt.colorbar(label='Concentration (normalized)')
+        plt.xlabel('Position along the channel (m)')
+        plt.ylabel('Time (s)')
+        plt.title('Advection-Diffusion of a Point Source')
+        plt.grid(True)
+        plt.show()
+'''
+
 
 def create_Unique_Dataset(number_bits):
     number_bits = 13  
     number_arrays = 2**number_bits 
 
+    # Create all possible combinations of 0 and 1 for the given number of bits
     sequenzes = np.array([list(map(int, format(i, f'0{number_bits}b'))) for i in range(number_arrays)])
 
     print(sequenzes.shape)
@@ -148,3 +219,7 @@ def create_Unique_Dataset(number_bits):
     assert len(sequenzes) == 2**number_bits, "Something went wrong, but i dont know why!"
     
     return sequenzes
+
+if __name__ == "__main__":
+    gen = DataGenerator()
+    gen.plot_a_sequence()
